b08c25d15c54b0f27c654d6bc6d0b9382ff944db
xDekann <106389146+xDekann@users.noreply.github.com>
2022-08-26 21:28:58 +0200

    Maven version of project - only for .jar creation

---
+++ snakefx/src/main/java/org/openjfx/snakefx/entities/FruitGenerator.java
@@ -0,0 +1,68 @@
+package org.openjfx.snakefx.entities;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.openjfx.snakefx.entities.Snake.Position;
+
+import javafx.scene.shape.Circle;
+import org.openjfx.snakefx.resources.*;
+
+public class FruitGenerator {
+
+	private Circle fruit;
+	private Position applePos;
+	private Random randomizer;
+	private ValueConfig constant;
+	
+	public FruitGenerator() {
+		
+		constant = ValueConfig.getInstance();
+
+		fruit = new Circle();
+		fruit.setRadius(constant.getFruitRadius());
+		fruit.setFill(constant.getFruitColor());
+		applePos = new Position();
+		randomizer = new Random();
+	}
+	
+	public void generateFruit(ArrayList<Position> segmentPos) {
+		Position pos;
+		applePos = new Position();
+		boolean rangeAccepted = false;
+		do {
+			
+			applePos.setX(constant.getMinFruitSpawnW()+
+					(randomizer.nextDouble()*
+							(constant.getMaxFruitSpawnW()-constant.getMinFruitSpawnW())));
+			applePos.setY(constant.getMinFruitSpawnH()+
+					(randomizer.nextDouble()*
+							(constant.getMaxFruitSpawnH()-constant.getMinFruitSpawnH())));
+			
+			for(int i=0;i<segmentPos.size();i++){
+				pos = segmentPos.get(i);
+				// checking if generated fruit pos is not in snake/is a bit away from snake 
+				// (fruit radius + segment radius * 2)
+				// using basic math formula for distance between two points
+				if(Math.sqrt(Math.pow(applePos.getX()-pos.getX(),2)+Math.pow(applePos.getY()-pos.getY(),2))<constant.getFruitGoodSpawn()) {
+					break;
+				}else if(Math.sqrt(Math.pow(applePos.getX()-pos.getX(),2)+Math.pow(applePos.getY()-pos.getY(),2))>=constant.getFruitGoodSpawn() && i==segmentPos.size()-1) {
+					//if the whole snake is checked successfully
+					rangeAccepted=true;
+				}
+				
+			}
+		}while(rangeAccepted==false);
+		fruit.setCenterX(applePos.getX());
+		fruit.setCenterY(applePos.getY());
+	}
+	
+
+	public Circle getFruit() {
+		return fruit;
+	}
+
+	public void setFruit(Circle fruit) {
+		this.fruit = fruit;
+	}
+}
---
+++ snakefx/src/main/java/org/openjfx/snakefx/game/LostMenu.java
@@ -0,0 +1,51 @@
+package org.openjfx.snakefx.game;
+
+import javafx.application.Platform;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class LostMenu {
+	
+	private VBox lostMenuBox;
+	private Text lostMessage;
+	private Button continueButton;
+	private Button endButton;
+	@SuppressWarnings("unused")
+	private SnakeGame game;
+	
+	public LostMenu(SnakeGame game) {
+		
+		lostMenuBox = new VBox();
+		lostMenuBox.setId("lostMenuBox");
+		
+		lostMessage = new Text("You lost!");
+		
+		continueButton = new Button("Try again");
+		continueButton.setId("buttonLost");
+		continueButton.setOnAction(e->{
+			game.setGameON(true);
+			game.initGame(game.getUserName(), game.getBestScore());
+		});
+		
+		endButton = new Button("Quit");
+		endButton.setId("buttonLost");
+		endButton.setOnAction(e->{
+			Platform.exit();
+		});
+		
+		
+	}
+	
+	public void initLost() {
+		lostMenuBox.getChildren().addAll(lostMessage,continueButton,endButton);
+	}
+
+	public VBox getLostMenuBox() {
+		return lostMenuBox;
+	}
+
+	public void setLostMenuBox(VBox lostMenuBox) {
+		this.lostMenuBox = lostMenuBox;
+	}
+}
